990b724bbc3c97d1ba0d2b97f6b27e267e101cc6
root <root@admin01.ci.its.hawaii.edu>
2021-03-26 05:41:57 +0000

    change in how the addition script works

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,16 +8,20 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
+oldcwd=`pwd`
 
 module load lmod
 spider  -o spider-json  ${MODULEPATH} | python  -mjson.tool  > ${DIR}/modules.json
-python ${DIR}/parse.py
-mv data.json ${DIR}/../data.json
+cd   ${DIR}
+python parse.py
 
+mv data.json ../data.json
+cd ..
 
 GIT_SSH_COMMAND="ssh -i ${DIR}/id_rsa" git pull --no-edit
-git add ${DIR}/../data.json
+git add data.json
 git commit -m "`date` modules updated"
 GIT_SSH_COMMAND="ssh -i ${DIR}/id_rsa" git push
 
-
+cd ${oldcwd}
+exit 0
